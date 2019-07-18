0549472c5b961c21f36e02a08c3527bd4d3828e2
dengpan0513 <1306631506@qq.com>
2019-07-18 16:43:43 +0800

    make button to a SPA after install parcel

---
+++ src/button.vue
@@ -0,0 +1,32 @@
+<template>
+  <button class="w-button">按钮</button>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style lang="scss">
+.w-button {
+        height: var(--button-height);
+        padding: 0 1em;
+        border: 1px solid var(--border-color);
+        border-radius: var(--border-radius);
+        background-color: var(--button-bg);
+        font-size: var(--font-size);
+
+        &:hover {
+          border-color: var(--border-color-hover);
+        }
+
+        &:active {
+          background: var(--button-active-bg);
+        }
+
+        &:focus {
+          outline: none
+        }
+      }
+</style>
