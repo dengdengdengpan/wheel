95e2e6ba06551fbed156c138cd090dd7b97010f6
dengdengdengpan <1306631506@qq.com>
2019-07-19 21:50:32 +0800

    add icon && iconPosition attribute in button

--- src/button.vue
+++ src/button.vue
@@ -1,32 +1,73 @@
 <template>
-  <button class="w-button">按钮</button>
+  <button class="w-button" :class="{[`icon-${iconPosition}`]: true}">
+    <svg v-if="icon" class="icon" aria-hidden="true">
+      <use :xlink:href="`#i-${icon}`"></use>
+    </svg>
+    <div class="content">
+      <slot></slot>
+    </div>
+  </button>
 </template>
 
 <script>
 export default {
-  
+  props: {
+    icon: {
+      type: String
+    },
+    iconPosition: {
+      type: String,
+      default: 'left'
+    }
+  }
 }
 </script>
 
 <style lang="scss">
 .w-button {
-        height: var(--button-height);
-        padding: 0 1em;
-        border: 1px solid var(--border-color);
-        border-radius: var(--border-radius);
-        background-color: var(--button-bg);
-        font-size: var(--font-size);
+  display: inline-flex;
+  justify-content: center;
+  align-items: center;
+  height: var(--button-height);
+  padding: 0 1em;
+  border: 1px solid var(--border-color);
+  border-radius: var(--border-radius);
+  background-color: var(--button-bg);
+  font-size: var(--font-size);
+  // 要使内联元素垂直方向对齐的方式
+  vertical-align: middle;
 
-        &:hover {
-          border-color: var(--border-color-hover);
-        }
+  &:hover {
+    border-color: var(--border-color-hover);
+  }
 
-        &:active {
-          background: var(--button-active-bg);
-        }
+  &:active {
+    background: var(--button-active-bg);
+  }
 
-        &:focus {
-          outline: none
-        }
-      }
+  &:focus {
+    outline: none
+  }
+
+  > .icon {
+    order: 1;
+    margin-right: .1em;
+  }
+
+  > .content {
+    order: 2; 
+  }
+
+  &.icon-right {
+    > .icon {
+      order: 2;
+      margin-left: .1em;
+      margin-right: 0;
+    }
+
+    > .content {
+      order: 1;
+    }
+  }
+}
 </style>
