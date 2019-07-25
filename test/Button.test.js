a30457e1d04960d7d086dfaaa0fac07138f0506c
dengdengdengpan <1306631506@qq.com>
2019-07-25 23:31:03 +0800

    使用 travis 持续集成

--- test/Button.test.js
+++ test/Button.test.js
@@ -9,6 +9,7 @@ describe('Button', () => {
   it('存在.', () => {
     expect(Button).to.be.ok
   })
+
   it('可以设置icon.', () => {
     const Constructor = Vue.extend(Button)
     const vm = new Constructor({
@@ -20,6 +21,7 @@ describe('Button', () => {
     expect(useElement.getAttribute('xlink:href')).to.equal('#i-settings')
     vm.$destroy()
   })
+
   it('可以设置loading.', () => {
     const Constructor = Vue.extend(Button)
     const vm = new Constructor({
@@ -33,6 +35,7 @@ describe('Button', () => {
     expect(useElements[0].getAttribute('xlink:href')).to.equal('#i-loading')
     vm.$destroy()
   })
+
   it('icon 默认的 order 是 1', () => {
     const div = document.createElement('div')
     document.body.appendChild(div)
@@ -62,6 +65,7 @@ describe('Button', () => {
     vm.$el.remove()
     vm.$destroy()
   })
+  
   it('点击 button 触发 click 事件', () => {
     const Constructor = Vue.extend(Button)
     const vm = new Constructor({
