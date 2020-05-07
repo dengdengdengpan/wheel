6092e93394ff3604c9d5f2be11c9d35bf35a711b
dengdengdengpan <1306631506@qq.com>
2020-05-07 23:06:50 +0800

    完成 WInput 基本样式

--- src/app.js
+++ src/app.js
@@ -2,10 +2,12 @@ import Vue from 'vue'
 import WButton from './components/WButton.vue'
 import WIcon from './components/WIcon.vue'
 import WButtonGroup from './components/WButtonGroup.vue'
+import WInput from './components/WInput.vue'
 
 Vue.component('w-button', WButton)
 Vue.component('w-icon', WIcon)
 Vue.component('w-button-group', WButtonGroup)
+Vue.component('w-input', WInput)
 
 new Vue({
   el: '#app',
--- src/components/WInput.vue
+++ src/components/WInput.vue
@@ -1,13 +1,65 @@
 <template>
-  <input type="text">
+  <div class="input-wrapper">
+    <input :value="value" :disabled="disabled" :readonly="readonly" type="text">
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'WInput'
+  name: 'WInput',
+  props: {
+    value: {
+      type: String
+    },
+    disabled: {
+      type: Boolean,
+      default: false
+    },
+    readonly: {
+      type: Boolean,
+      default: false
+    }
+  }
 }
 </script>
 
 <style lang="scss" scoped>
+$height: 32px;
+$border-color: #666;
+$border-color-hover: #999;
+$border-radius: 4px;
+$font-size: 14px;
+$color: #111;
+$box-shadow-color: rgba(0, 0, 0, .5);
 
+.input-wrapper {
+  display: inline-block;
+  vertical-align: middle;
+  font-size: $font-size;
+
+  > input {
+    height: $height;
+    padding: 0 8px;
+    border-radius: $border-radius;
+    border: 1px solid $border-color;
+    font-size: inherit;
+    color: $color;
+
+    &:hover {
+      border-color: $border-color-hover;
+    }
+
+    &:focus {
+      outline: none;
+      box-shadow: inset 0 1px 3px $box-shadow-color;
+    }
+
+    &:disabled {
+      border-color: #e4e7ed;
+      background-color: #f5f7fa;
+      color: #c0c4cc;
+      cursor: not-allowed;
+    }
+  }
+}
 </style>
