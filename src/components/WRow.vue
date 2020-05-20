df4c89c2985e6e85e9a3e160d56a9580c7e2eb90
dengdengdengpan <1306631506@qq.com>
2020-05-21 00:18:31 +0800

    Grid Row props 新增 justify

--- src/components/WRow.vue
+++ src/components/WRow.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="w-row" :style="rowStyle">
+  <div class="w-row" :style="rowStyle" :class="rowClass">
     <slot></slot>
   </div>
 </template>
@@ -10,6 +10,12 @@ export default {
   props: {
     gutter: {
       type: Number
+    },
+    justify: {
+      type: String,
+      validator (value) {
+        return ['start', 'end', 'center', 'space-between', 'space-around', 'space-evenly'].includes(value)
+      }
     }
   },
   computed: {
@@ -19,6 +25,12 @@ export default {
         marginLeft: (-gutter / 2) + 'px',
         marginRight: (-gutter / 2) + 'px'
       }
+    },
+    rowClass () {
+      const { justify } = this
+      return [
+        justify && `w-row-${justify}`
+      ]
     }
   },
   mounted () {
@@ -35,5 +47,29 @@ export default {
   /* 如果一个 row 中的 col 总和超过 24，那么多余的 col 会作为一个整体另起一行排列 */
   flex-flow: row wrap;
   margin-bottom: 8px;
+
+  &.w-row-start {
+    justify-content: flex-start;
+  }
+
+  &.w-row-end {
+    justify-content: flex-end;
+  }
+
+  &.w-row-center {
+    justify-content: center;
+  }
+
+  &.w-row-space-between {
+    justify-content: space-between;
+  }
+
+  &.w-row-space-around {
+    justify-content: space-around;
+  }
+
+  &.w-row-space-evenly {
+    justify-content: space-evenly;
+  }
 }
 </style>
